cf76a37ad9a5cfac5e85e17ffaf2428dba13f4c6
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2025-03-10 18:24:12 +0300

    Update select.sql

--- Hafta2/select.sql
+++ Hafta2/select.sql
@@ -1,1 +1,9 @@
 
+--AÇIKLAMA SATIRI. SQL KOMUTU DEĞİL
+USE tempdb--HANGİ VERİTABANINI KULLANACAĞIMIZI BELİRTİRİZ.
+GO
+USE master
+GO
+SELECT optname FROM master.DBO.MSreplication_options
+SELECT * FROM master.DBO.MSreplication_options
+GO
